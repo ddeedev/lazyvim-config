f2835cc12878f28bd164eadd127946aad70c04cb
dDeeDev <dee.hamdee.084@gmail.com>
2024-04-25 20:39:01 +0700

    :arrow_up: add more plugin

--- lua/plugins/toggle-term.lua
+++ lua/plugins/toggle-term.lua
@@ -2,7 +2,7 @@ return {
   "akinsho/toggleterm.nvim",
   config = function()
     require("toggleterm").setup({
-      open_mapping = "<leader>fd",
+      open_mapping = "<leader>tt",
       shade_terminals = false,
       -- add --login so ~/.zprofile is loaded
       -- https://vi.stackexchange.com/questions/16019/neovim-terminal-not-reading-bash-profile/16021#16021
@@ -10,12 +10,12 @@ return {
     })
   end,
   keys = {
-    { "<leader>fd" },
+    { "<leader>tt" },
     { "<leader>0", "<Cmd>2ToggleTerm<Cr>", desc = "Terminal #2" },
     {
       "<leader>td",
-      "<cmd>ToggleTerm size=40 dir=~/Desktop direction=horizontal<cr>",
-      desc = "Open a horizontal terminal at the Desktop directory",
+      "<cmd>ToggleTerm size=40 direction=horizontal<cr>",
+      desc = "Open a large horizontal terminal",
     },
   },
 }
