6d636773fe4e20cc8e9c29de432d6695e170d54f
dDeeDev <dee.hamdee.084@gmail.com>
2024-04-26 14:38:47 +0700

    :bug: fix toggleterm working on insert mode

--- lua/plugins/toggle-term.lua
+++ lua/plugins/toggle-term.lua
@@ -2,7 +2,7 @@ return {
   "akinsho/toggleterm.nvim",
   config = function()
     require("toggleterm").setup({
-      open_mapping = "<leader>tt",
+      --    open_mapping = "<leader>fd",
       shade_terminals = false,
       -- add --login so ~/.zprofile is loaded
       -- https://vi.stackexchange.com/questions/16019/neovim-terminal-not-reading-bash-profile/16021#16021
@@ -10,9 +10,10 @@ return {
     })
   end,
   keys = {
-    { "<leader>tt" },
-    { "<leader>0", "<Cmd>2ToggleTerm<Cr>", desc = "Terminal #2" },
+    { mode = { "n" }, "<leader>tt","<Cmd>ToggleTerm<Cr>" ,desc = "Toggle Primary Terminal" },
+    { mode = { "n" }, "<leader>0", "<Cmd>2ToggleTerm<Cr>", desc = "Terminal #2" },
     {
+      mode = { "n" },
       "<leader>td",
       "<cmd>ToggleTerm size=40 direction=horizontal<cr>",
       desc = "Open a large horizontal terminal",
