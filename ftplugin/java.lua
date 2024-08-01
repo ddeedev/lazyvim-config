f53ded92ce0b15d69b7f5408d59fd353949ee42c
dDeeDev <dee.hamdee.084@gmail.com>
2024-08-02 01:50:52 +0700

    :bug: fix path

---
+++ ftplugin/java.lua
@@ -0,0 +1,97 @@
+local home = os.getenv("HOME")
+local workspace_path = home .. "/.local/share/nvim/jdtls-workspace/"
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+local workspace_dir = workspace_path .. project_name
+
+local status, jdtls = pcall(require, "jdtls")
+if not status then
+  return
+end
+local extendedClientCapabilities = jdtls.extendedClientCapabilities
+
+local config = {
+  cmd = {
+    "java",
+    "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+    "-Dosgi.bundles.defaultStartLevel=4",
+    "-Declipse.product=org.eclipse.jdt.ls.core.product",
+    "-Dlog.protocol=true",
+    "-Dlog.level=ALL",
+    "-Xmx1g",
+    "--add-modules=ALL-SYSTEM",
+    "--add-opens",
+    "java.base/java.util=ALL-UNNAMED",
+    "--add-opens",
+    "java.base/java.lang=ALL-UNNAMED",
+    "-javaagent:" .. home .. "/.local/share/nvim/mason/packages/jdtls/lombok.jar",
+    "-jar",
+    vim.fn.glob(home .. "/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"),
+    "-configuration",
+    home .. "/.local/share/nvim/mason/packages/jdtls/config_mac",
+    "-data",
+    workspace_dir,
+  },
+  root_dir = require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }),
+
+  settings = {
+    java = {
+      signatureHelp = { enabled = true },
+      extendedClientCapabilities = extendedClientCapabilities,
+      maven = {
+        downloadSources = true,
+      },
+      configuration = {
+        updateBuildConfiguration = "interactive",
+        -- TODO Update this by adding any runtimes that you need to support your Java projects and removing any that you don't have installed
+        -- The runtime name parameters need to match specific Java execution environments.  See https://github.com/tamago324/nlsp-settings.nvim/blob/2a52e793d4f293c0e1d61ee5794e3ff62bfbbb5d/schemas/_generated/jdtls.json#L317-L334
+        runtimes = {
+          {
+            name = "JavaSE-21",
+            path = "/Library/Java/JavaVirtualMachines/jdk-21.jdk/Contents/Home/",
+          },
+        },
+      },
+      referencesCodeLens = {
+        enabled = true,
+      },
+      references = {
+        includeDecompiledSources = true,
+      },
+      inlayHints = {
+        parameterNames = {
+          enabled = "all", -- literals, all, none
+        },
+      },
+      format = {
+        enabled = false,
+      },
+    },
+  },
+
+  init_options = {
+    bundles = {},
+  },
+}
+require("jdtls").start_or_attach(config)
+
+vim.keymap.set("n", "<leader>co", "<Cmd>lua require'jdtls'.organize_imports()<CR>", { desc = "Organize Imports" })
+vim.keymap.set("n", "<leader>crv", "<Cmd>lua require('jdtls').extract_variable()<CR>", { desc = "Extract Variable" })
+vim.keymap.set(
+  "v",
+  "<leader>crv",
+  "<Esc><Cmd>lua require('jdtls').extract_variable(true)<CR>",
+  { desc = "Extract Variable" }
+)
+vim.keymap.set("n", "<leader>crc", "<Cmd>lua require('jdtls').extract_constant()<CR>", { desc = "Extract Constant" })
+vim.keymap.set(
+  "v",
+  "<leader>crc",
+  "<Esc><Cmd>lua require('jdtls').extract_constant(true)<CR>",
+  { desc = "Extract Constant" }
+)
+vim.keymap.set(
+  "v",
+  "<leader>crm",
+  "<Esc><Cmd>lua require('jdtls').extract_method(true)<CR>",
+  { desc = "Extract Method" }
+)
