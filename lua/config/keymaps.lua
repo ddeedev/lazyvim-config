346e0f11e7467e7ebf756f13fa8659d9e4e05935
dDeeDev <dee.hamdee.084@gmail.com>
2025-01-08 14:12:15 +0700

    feat(copilot): add copilot and avante

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,3 +7,19 @@ vim.keymap.del("n", "<leader>ft")
 vim.keymap.del("n", "<leader>fT")
 vim.api.nvim_set_keymap("n", "<leader>e", ":Neotree toggle float<CR>", {desc = "Float File Explorer", silent = true} )
 vim.api.nvim_set_keymap("n", "<leader>r", ":Neotree toggle right<CR>", {desc = "Right File Explorer", silent = true} )
+
+-- toggle copilot
+-- Function to toggle Copilot
+function _G.toggle_copilot()
+  local copilot_status = vim.fn["copilot#Enabled"]()
+  if copilot_status == 1 then
+    vim.cmd("Copilot disable")
+    vim.notify("Copilot disabled", vim.log.levels.INFO, {title = "Copilot"})
+  else
+    vim.cmd("Copilot enable")
+    vim.notify("Copilot enabled", vim.log.levels.INFO, {title = "Copilot"})
+  end
+end
+
+-- Toggle Copilot with <leader>ce
+vim.api.nvim_set_keymap("n", "<leader>ce", ":lua _G.toggle_copilot()<CR>", {desc = "Toggle Copilot", silent = false})
--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -5,6 +5,43 @@ return {
   version = false, -- set this if you want to always pull the latest change
   opts = {
     -- add any opts here
+    provider = "copilot",
+    auto_suggestions_provider = "copilot",
+
+    -- keymap
+    mappings = {
+      --- @class AvanteConflictMappings
+      diff = {
+        ours = "co",
+        theirs = "ct",
+        all_theirs = "ca",
+        both = "cb",
+        cursor = "cc",
+        next = "]x",
+        prev = "[x",
+      },
+      suggestion = {
+        accept = "<M-l>",
+        next = "<M-]>",
+        prev = "<M-[>",
+        dismiss = "<C-]>",
+      },
+      jump = {
+        next = "]]",
+        prev = "[[",
+      },
+      submit = {
+        normal = "<CR>",
+        insert = "<C-S>",
+      },
+      sidebar = {
+        apply_all = "A",
+        apply_cursor = "a",
+        switch_windows = "<Tab>",
+        reverse_switch_windows = "<S-Tab>",
+      },
+    },
+    hints = { enabled = false },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = "make",
