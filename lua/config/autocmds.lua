c9a737f7b098eae0f95eeaad71bdfad7ba720070
dDeeDev <dee.hamdee.084@gmail.com>
2025-01-10 17:52:53 +0700

    disalbe copilot on startup

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,12 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Disable Copilot by default on startup
+vim.api.nvim_create_autocmd("VimEnter", {
+  desc = "Disable Copilot by default on startup",
+  callback = function()
+    vim.cmd("Copilot disable")
+    vim.g.copilot_enabled = false
+  end,
+})
