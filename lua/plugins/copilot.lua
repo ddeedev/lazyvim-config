7d45aaa72c2775b50f557b6aa396f5636f128eb1
ddeedev <dee.hamdee.084@gmail.com>
2025-11-30 18:10:29 +0700

    feat(plugin): add vcode-diff plugin

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,5 +1,6 @@
 return {
   "github/copilot.vim",
+  event = "VeryLazy",
   -- config = function()
   --   vim.g.copilot_model = "sonnet 3.5"
   -- end,
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,17 +1,12 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    opts = function(_, opts)
-      -- preserve existing options
-      opts.inlay_hints = opts.inlay_hints or {}
-
-      -- modify specific settings
-      opts.inlay_hints.enabled = false
-
-      -- you can modify other settings here as needed
-      -- for example:
-      -- opts.autoformat = false
-      return opts
-    end,
+    init_options = {
+      userLanguages = {
+        eelixir = "html-eex",
+        eruby = "erb",
+        rust = "html",
+      },
+    },
   },
 }
